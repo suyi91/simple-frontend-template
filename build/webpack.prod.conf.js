d58a70f591102a46ac6229a460ce309b720d1411
Sean <sy170107@gmail.com>
2018-01-22 11:39:43 +0800

    webpack(>= 3)使用uglifyjs-webpack-plugin压缩代码

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -5,19 +5,20 @@ process.env.NODE_ENV = 'production';
 const webpack = require('webpack');
 const Merge = require('webpack-merge');
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 let config = require('./webpack.base.conf');
 
 config = Merge(config, {
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        }
       },
       sourceMap: true,
-      comments: false,
-      parallel: true,
+      parallel: true
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
